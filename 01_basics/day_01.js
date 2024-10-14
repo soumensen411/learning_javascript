b7235c801ab210984f557644f0b65d212ff9c55e
Soumen sen <144879914+soumensen411@users.noreply.github.com>
2024-10-14 13:27:50 +0000

    variable and constant

--- 01_basics/day_01.js
+++ 01_basics/day_01.js
@@ -1,1 +1,6 @@
-console.log("soumen sen")
+//variable and constant 
+
+const accountId = 124578
+var accountEmail = "soumen@gmail.com"
+let accountState = "Chittagong"
+console.table([accountId, accountEmail, accountState])
